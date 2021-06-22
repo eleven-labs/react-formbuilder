f1c9eb21dbe7c26858b26c8ca93ee0d0038dd3bb
PASQUET Fabien <fab.pasquet@gmail.com>
2021-06-22 15:33:51 +0200

    Version 0.2.7

--- src/validations.js
+++ src/validations.js
@@ -1,16 +1,12 @@
-import isEmpty from "lodash/isEmpty";
+import isEmpty from 'lodash/isEmpty';
 
 export default {
-  Required: v => {
-    if (typeof v === "number") {
-      return true;
-    }
-    return !isEmpty(v);
-  },
+  Required: v => !isEmpty(v),
   Email: email => {
     const regex = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
-    return regex.test(email);
+
+    return email && email.length > 0 ? regex.test(email) : true;
   },
   IsGreaterThan: (v, { length }) => v.length >= length,
-  RepeatValue: (v, { values, key }) => v === values[key]
+  RepeatValue: (v, { values, key }) => v === values[key],
 };
